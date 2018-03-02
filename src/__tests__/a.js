ec366fee56cfa36604ce70da0f9c70449ea67d15
Thomas Hallock <hallock@gmail.com>
2018-03-02 01:08:06 -0800

    full automock

--- src/__tests__/a.js
+++ src/__tests__/a.js
@@ -1,15 +1,8 @@
 const a = require('../a')
-jest.mock('../a', () => {
-	const unmockedA = jest.genMockFromModule('../a')
-	return class extends unmockedA {
-		foo() {
-			return "mocked foo function"
-		}
-	}
-})
+jest.mock('../a')
 describe('a module', function () {
 	it('should be mocked', function () {
-		expect(new a().foo()).toEqual('mocked foo function')
+		expect(new a().foo()).toEqual(undefined)
 		expect(new a().bar()).toEqual(undefined)
 	})
 })
