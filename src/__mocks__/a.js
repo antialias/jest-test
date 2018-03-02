8c3b91043a17a1e81307ea187db40e43002bedd3
Thomas Hallock <hallock@gmail.com>
2018-03-02 01:02:26 -0800

    local mocks dont inherit from unmocked versions

--- src/__mocks__/a.js
+++
@@ -1,8 +0,0 @@
-module.exports = class {
-	foo() {
-		return "mocked foo function"
-	}
-	bar() {
-		return "mocked bar function"
-	}
-}
--- src/__tests__/a.js
+++ src/__tests__/a.js
@@ -1,7 +1,14 @@
 const a = require('../a')
+jest.mock('../a', () => {
+	return class {
+		foo() {
+			return "mocked foo function"
+		}
+	}
+})
 describe('a module', function () {
 	it('should be mocked', function () {
 		expect(new a().foo()).toEqual('mocked foo function')
-		expect(new a().bar()).toEqual('mocked bar function')
+		expect(new a().bar).toBeUndefined();
 	})
 })
